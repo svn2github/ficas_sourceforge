9af78b5e7588f65561cf352e9dd66e1ca50a6bae
whebisch <whebisch@b0c55286-4f34-0410-a049-a1e7e93b0762>
2008-06-23 21:23:43 +0000

    Tag 1.0.3 release.
    git-svn-id: https://svn.code.sf.net/p/fricas/code@302 b0c55286-4f34-0410-a049-a1e7e93b0762

---
+++ releases/1.0.3/src/lisp/fricas-package.lisp
@@ -0,0 +1,35 @@
+;;; We put this in separate file to avoid problems with compilation.
+(eval-when (:execute :compile-toplevel :load-toplevel)
+(if (not (find-package "FRICAS-LISP"))
+   (make-package "FRICAS-LISP" 
+     :use (list (or (find-package "COMMON-LISP")
+                    "LISP"))))) 
+#+:sbcl
+(eval-when (:execute :compile-toplevel :load-toplevel)
+    (setf SB-IMPL::*DEFAULT-EXTERNAL-FORMAT* :LATIN-1))
+
+(in-package "FRICAS-LISP")
+(do-symbols (x "FRICAS-LISP") (export (list x)))
+
+(export '(quit chdir |getEnv| |load_quietly| get-current-directory
+          fricas-probe-file trim-directory-name pad-directory-name
+          file-kind makedir fricas-compile-file load-maybe-compiling
+          maybe-compile
+          |writeablep| |openServer| |sockGetInt|
+          |sockSendInt| |sockSendString| |sockGetFloat| |sockSendFloat|
+          |serverSwitch| |sockSendSignal| |sockGetStringFrom|))
+
+#+:GCL
+(progn
+    (import '(LISP::LAMBDA-CLOSURE))
+    (export '(LISP::LAMBDA-CLOSURE))
+)
+#+:ecl 
+(progn
+    (require 'cmp)
+    (eval-when (:execute :compile-toplevel :load-toplevel)
+         (proclaim '(optimize (safety 1))))
+)
+#+:openmcl
+(eval-when (:execute :compile-toplevel :load-toplevel)
+      (setf *features* (delete :CCL *features*)))
